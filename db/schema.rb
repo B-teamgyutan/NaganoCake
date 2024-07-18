1fa561b4cb0107b1670c56bd182f43db80211abe
EC2 Default User <ec2-user@ip-172-31-2-186.ap-northeast-1.compute.internal>
2024-07-18 07:21:39 +0000

    ヘッダー作成

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2024_07_17_064707) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
