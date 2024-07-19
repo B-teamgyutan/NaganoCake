234b0e63f17166858afa73f38fdbef3a41692ccb
EC2 Default User <ec2-user@ip-172-31-2-186.ap-northeast-1.compute.internal>
2024-07-19 04:10:13 +0000

    7/19日migrate修正分

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2024_07_18_040931) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -38,7 +37,6 @@ ActiveRecord::Schema.define(version: 2024_07_18_040931) do
     t.string "post_code"
     t.string "address"
     t.string "phone_number"
-
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -47,6 +45,15 @@ ActiveRecord::Schema.define(version: 2024_07_18_040931) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "item_details", null: false
+    t.integer "price", null: false
+    t.boolean "is_sold_out", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
