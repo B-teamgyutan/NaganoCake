ad0ff6a94ce01842ce43bee5079e2170d6e164e2
sae usudate <171134641+saeusu@users.noreply.github.com>
2024-07-21 06:24:35 +0000

    orders 更新

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,9 @@ belongs_to :customer
 validates :post_code, presence: true, length: { maximum: 7, minimun: 7 },numericality: true
 validates :address, presence: true
 validates :name, presence: true
+
+  def address_display
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
+
 end
