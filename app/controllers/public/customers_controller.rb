5f7ab026dcdacbb0ebf521e4c44410e6347532b7
ck-nm <168258507+ck-nm@users.noreply.github.com>
2024-07-22 09:25:48 +0000

    マイページと退会

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,21 +1,29 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+
   def update
     customer = current_customer
     customer.update(customer_params)
     radirect_to customers_mypage_path
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).parmit( :last_name,
                                       :first_name,
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_21_064233) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.string "post_code", null: false
-    t.string "address", null: false
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2024_07_22_064357) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -47,15 +38,9 @@ ActiveRecord::Schema.define(version: 2024_07_21_064233) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "post_code"
-    t.string "address"
-    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
@@ -71,6 +56,15 @@ ActiveRecord::Schema.define(version: 2024_07_21_064233) do
     t.text "item_details", null: false
     t.integer "price", null: false
     t.boolean "is_sold_out", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mailing_addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
