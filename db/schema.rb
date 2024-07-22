10e809319c79fcd4f0cd9ded5d1e2d2fded37213
sae usudate <171134641+saeusu@users.noreply.github.com>
2024-07-22 09:03:56 +0000

    new.htmlの更新2

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2024_07_21_064233) do
 
+create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+  
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,7 +74,6 @@ ActiveRecord::Schema.define(version: 2024_07_21_064233) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id　"
