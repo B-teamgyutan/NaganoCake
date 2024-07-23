7ac4b320897eb06bf5be68343904c28fdad1ab61
sae usudate <171134641+saeusu@users.noreply.github.com>
2024-07-23 14:11:02 +0000

    Add admin/order_details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_22_064357) do
+ActiveRecord::Schema.define(version: 2024_07_23_113639) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -72,6 +72,16 @@ ActiveRecord::Schema.define(version: 2024_07_22_064357) do
     t.text "item_details", null: false
     t.integer "price", null: false
     t.boolean "is_sold_out", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_datails", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
