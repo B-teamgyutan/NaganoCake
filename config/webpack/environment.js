2cdf3d37d47af8424a5bd8b0d06ca4bad53e4a1e
sae usudate <171134641+saeusu@users.noreply.github.com>
2024-07-23 06:10:27 +0000

    confirmの更新

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,9 +6,8 @@ const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    Popper: ['popper.js', 'default']
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
   })
 )
