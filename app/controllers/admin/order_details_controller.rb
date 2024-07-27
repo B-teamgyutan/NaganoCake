f7d4395434302d15a01427413558790ef3955056
sae usudate <171134641+saeusu@users.noreply.github.com>
2024-07-27 06:55:38 +0000

    order修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -11,6 +11,6 @@ class Admin::OrderDetailsController < ApplicationController
 
 	private
 		def order_detail_params
-			params.require(:order_detail).permit(:making_status)
+			params.require(:order_detail).permit(:order_id, :item_id, :price, :amount, :making_status)
 		end
 end
